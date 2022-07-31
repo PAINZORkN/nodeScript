cb05c991c39825f1a25c259adf8217e8c8672bfa
PAINZOR <painzor@outlook.it>
2022-08-01 01:12:46 +0200

    feat: added exercise

---
+++ Promises lucky draw/index.mjs
@@ -0,0 +1,22 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+
+
+  luckyDraw("Joe")
+  .then((data) => console.log(data))
+  .then(() => luckyDraw("Sabrina"))
+  .then((data) => console.log(data))
+  .then(() => luckyDraw("Carolina"))
+  .then((data) => console.log(data))
+  .catch(err => console.error(err))
