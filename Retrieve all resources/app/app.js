c279bcaeda96144c1a04942691cb28f38bfa806e
PAINZOR <painzor@outlook.it>
2022-08-20 17:55:54 +0200

    feat: added exercise

---
+++ Retrieve all resources/app/app.js
@@ -0,0 +1,60 @@
+app.get('/planets/:id(\\d+)', async (req, res, next) => {
+    const planetId = Number(req.params.id);
+  
+    const planet = await prisma.planet.findUnique({
+      where: { id: planetId },
+    });
+  
+    if (!planet) {
+      res.status(404);
+      return next(`Cannot GET /planet/${planetId}`);
+    }
+    res.json(planet);
+  });
+
+  app.post('/planets', validate({ body: planetSchema }), async (req, res) => {
+    const planetData = req.body 
+
+    res.status(201).json(planet);
+
+    app.put(
+        '/planets/:id(\\d+)',
+        validate({ body: planetSchema }),
+        async (req, res, next) => {
+          const planetData = req.body;
+          const planetId = Number(req.params.id);
+      
+          try {
+            const planet = await prisma.planet.update({
+              where: { id: planetId },
+              data: planetData,
+            });
+          } catch (err) {
+            res.status(404);
+            next(`Cannot PUT /planets/${planetId}`);
+          }
+      
+          res.status(200).json(planet);
+        }
+      );
+      
+      app.delete('/planets/:id(\\d+)', async (req, res, next) => {
+        const planetId = Number(req.params.id);
+      
+        try {
+          await prisma.planet.delete({
+            where: { id: planetId },
+          });
+        } catch (err) {
+          res.status(404);
+          next(`Cannot DELETE /planets/${planetId}`);
+        }
+      
+        res.status(204).end();
+      });
+      app.use(validationErrorMiddleware);
+
+
+})
+
+module.exports = app;
