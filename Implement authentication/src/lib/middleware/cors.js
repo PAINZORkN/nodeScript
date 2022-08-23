a702cd98accbafc0dd3c688aff2854fc84932da5
PAINZOR <painzor@outlook.it>
2022-08-24 00:01:29 +0200

    add exercise

---
+++ Implement authentication/src/lib/middleware/cors.js
@@ -0,0 +1,12 @@
+const cors = require('cors');
+
+const initCorsMiddleware = () => {
+  const corsOption = {
+    origin: 'http://localhost:3000',
+    credential: true,
+  };
+
+  return cors(corsOption);
+};
+
+module.exports = initCorsMiddleware;
